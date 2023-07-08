89ad31c6506ec3f337ceb6d234edd338194469ce
Abhinav Nath <abhinavnath@ymail.com>
2023-07-09 00:04:00 +0530

    Initialise repository

---
+++ src/main/java/com/codecafe/hellospark/HelloSparkApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.hellospark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSparkApplication.class, args);
+	}
+
+}
